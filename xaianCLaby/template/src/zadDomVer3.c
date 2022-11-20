734e53c8ecb6c37bb3b3acd8cc6cb199e3ae6e06
N0r3b0 <wfedde@gmail.com>
2022-11-21 00:06:39 +0100

    xaian zaddom update

--- xaianCLaby/template/src/zadDomVer3.c
+++ xaianCLaby/template/src/zadDomVer3.c
@@ -2,15 +2,21 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-char *replaceFnc(char *str, char sep)
+char *replaceFnc(char *str, char sep, uint8_t option) //option == true --> change letters to small or large
 {
     int len = strlen(str);
     int counter = 0;
     char *replacedStr = malloc(len * sizeof(char));
     while (*str)
     {
-        if (*str >= 'A' && *str <= 'Z' || *str >= 'a' && *str <= 'z')
+        if (option == true && *str >= 'A' && *str <= 'Z')
+            *replacedStr = *str+32; //+32 for replacing small letters with capitals
+        else if(option == true && *str >= 'a' && *str <= 'z')
+            *replacedStr = *str-32; //-32 for replacing capital letters with smalls
+
+        else if(option == false && *str >= 'A' && *str <= 'Z' || *str >= 'a' && *str <= 'z')
             *replacedStr = *str;
         else if (*(replacedStr - 1) == sep)
         {
@@ -33,8 +39,8 @@ int main()
 {
     char *string1 = "p.rzyk;l@adowy$*  string";
     char sep = '-';
-    char *string2 = replaceFnc(string1, sep);
-    
+    bool option = true;
+    char *string2 = replaceFnc(string1, sep, false);
     printf(string2);
     return 0;
 }
