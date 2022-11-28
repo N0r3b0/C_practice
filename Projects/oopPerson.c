2d53217339759dce64baaa5193d2bca119c0cf30
N0r3b0 <wfedde@gmail.com>
2022-11-28 22:05:04 +0100

    oopPerson finished

--- Projects/oopPerson.c
+++ Projects/oopPerson.c
@@ -1,53 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define nameLength 32    //length of strings made for names
 
 typedef struct Person
 {
-    char name[128];
-    char lastname[128];
+    char name[nameLength];
+    char lastname[nameLength];
     int age;
-    void (*setName)(struct Person *, char[128]);
-    char (*getName)(struct Person *);
+    void (*setName)(struct Person *, char[nameLength]);
+    void (*setLastName)(struct Person *, char[nameLength]);
     void (*setAge)(struct Person *, int);
-    int (*getAge)(struct Person *);
+    char* (*toString)(struct Person *);
     
 } Person;
 
-void setName_f(Person *person, char newName[128])
+void setName_f(Person *person, char newName[nameLength])
 {
-    for (int i = 0; i < 128; i++)
+    for (int i = 0; i < nameLength; i++)
         person->name[i] = newName[i];
 }
-char getName_f(Person *person)
+void setLastName_f(Person *person, char newLastName[nameLength])
 {
-    return *person->name;
+    for (int i = 0; i < nameLength; i++)
+        person->lastname[i] = newLastName[i];
 }
 void setAge_f(Person *person, int newAge)
 {
     person->age = newAge;
 }
-int getAge_f(Person *person)
+
+char *personToStr_f(Person *person)
 {
-    return person->age;
+    char *output = malloc(nameLength * 2 * sizeof(char) + sizeof(int) + 16); // 16 --> buffer for whitespaces and extra characters
+    int i = 0;
+    int counter = 0;
+
+    while (person->name[i] != '\0')
+    {
+        output[counter] = person->name[i];
+        counter++;
+        i++;
+    }
+
+    output[counter] = ' ';    //adding whitespace
+    counter++;
+
+    i = 0;  //i must be 0 before next loop
+    while (person->lastname[i] != '\0')
+    {
+        output[counter] = person->lastname[i];
+        counter++;
+        i++;
+    }
+
+    output[counter] = ' ';
+    counter++;
+
+    char *tempAge = malloc(3*sizeof(char));    //temp for itoa, 3 digits for age
+    itoa(person->age, tempAge, 10);  //int to string
+    i = 0;   // i must be 0 before next loop
+    while (i<2)
+    {
+        output[counter] = tempAge[i];
+        counter++;
+        i++;
+    }
+
+    output[counter] = '\0';
+    return output;
 }
+
 int main(void)
 {
-    Person adam;
-    adam.setAge = setAge_f;
-    adam.setName = setName_f;
-    adam.getAge = getAge_f;
-    adam.getName = getName_f;
+    Person person1;
+    //connecting functions to Person struct
+    person1.setName = setName_f;
+    person1.setLastName = setLastName_f;
+    person1.setAge = setAge_f;
+    person1.toString = personToStr_f;
+    //setting variables
+    person1.setName(&person1, "Adam");
+    person1.setLastName(&person1, "Kowalski");
+    person1.setAge(&person1, 14);
 
-    adam.setName(&adam, "Przykladowe imie");
-    printf("Adam's name is %s\n", adam.name);
+    printf("%s", person1.toString(&person1));
+    
+    printf("\n");
 
-    adam.setName(&adam, "Adam");
-    printf("Adam's name is %s\n", adam.getName(&adam));
+    //new variables
+    person1.setName(&person1, "Alexey");
+    person1.setLastName(&person1, "Isalov");
+    person1.setAge(&person1, 14);
 
-    adam.setAge(&adam, 30);
-    printf("Adam's age is %d\n", adam.age);
-
-    adam.setAge(&adam, 18);
-    printf("Adam's age is %d\n", adam.getAge(&adam));
+    printf("%s", person1.toString(&person1));
 
     return 0;
 }
